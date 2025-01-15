36130ad7f2f1f9555479ecde226881c423be7b27
leodipaula <leofernandes9@gnail.com>
2025-01-14 23:17:44 -0300

    First commit with spring structure

---
+++ src/main/java/github/com/leodipaula/PartyOrganizer/PartyOrganizerApplication.java
@@ -0,0 +1,13 @@
+package github.com.leodipaula.PartyOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/leodipaula/PartyOrganizer/PartyOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.leodipaula.PartyOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
